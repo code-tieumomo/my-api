8343bcc4ce82463c3a9aaaa501f568c9656f81a2
code-tieumomo <code.tieumomo@gmail.com>
2022-03-13 08:05:54 +0700

    Ready for ubuntu

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::fallback(function() {
+    return response()->json([
+        'name' => '(づ￣ 3￣)づ',
+        'logo' => '༼ つ ◕_◕ ༽つ',
+    ]);
 });
