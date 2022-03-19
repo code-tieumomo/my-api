3b28dc2491bece1bd4e11af818dda2accb4537ea
code-tieumomo <code.tieumomo@gmail.com>
2022-03-19 17:10:09 +0700

    Get current user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function() {
-//    Route::get('/user', function(Request $request) {
-//        return $request->user();
-//    });
+    Route::get('/user', function() {
+        return Auth::user();
+    });
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 //    Route::resource('/items', ItemController::class);
